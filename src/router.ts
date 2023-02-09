1d8f7ec4e573f546c6d5ed8c7ccdb90970d9ce73
AdrianoHeller <adriano@technoizz.com>
2023-02-08 22:28:49 -0300

    fix: remove unexistent dep

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { HandleValidMethod } from './handlers';
 import { IRouter } from './interfaces';
 import { conn } from './external/dbConn';
-import { GetAllUsers, GetSingleUserByID } from './service';
+import { GetAllUsers } from './service';
 import {
 	FileNotFound,
 	HandleResponse,
@@ -25,48 +25,7 @@ export const Router: IRouter = {
 			HandleResponse(res, 500, err, 'error');
 		}
 	},
-	'/user': async (customReq, res): Promise<any> => {
-		try {
-			const { query } = customReq;
-			const id = query.get('id');
-			if (id?.length! <= 0) {
-				HandleResponse(res, 400, 'You should provide a id.', 'error');
-			}
-			const getUser = await GetSingleUserByID(
-				conn,
-				'bank',
-				'users',
-				new ObjectId(id!),
-			);
-			HandleResponse(res, 200, getUser, 'data');
-		} catch (err) {
-			HandleResponse(res, 500, err, 'error');
-		}
-	},
-	'/user/create': async (_, res): Promise<any> => {
-		try {
-			const getUsers = await GetAllUsers(conn, 'bank', 'users');
-			HandleResponse(res, 200, getUsers, 'data');
-		} catch (err) {
-			HandleResponse(res, 500, err, 'error');
-		}
-	},
-	'/user/update': async (_, res): Promise<any> => {
-		try {
-			const getUsers = await GetAllUsers(conn, 'bank', 'users');
-			HandleResponse(res, 200, getUsers, 'data');
-		} catch (err) {
-			HandleResponse(res, 500, err, 'error');
-		}
-	},
-	'/user/delete': async (_, res): Promise<any> => {
-		try {
-			const getUsers = await GetAllUsers(conn, 'bank', 'users');
-			HandleResponse(res, 200, getUsers, 'data');
-		} catch (err) {
-			HandleResponse(res, 500, err, 'error');
-		}
-	},
+
 	'/notFound': (_, res) => {
 		HandleResponse(res, 404, FileNotFound, 'message');
 	},
